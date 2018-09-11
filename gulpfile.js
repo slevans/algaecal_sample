85a16f5fe8fe456370a9b38ec87d9cdef946933e
Shelley Evans <shelley@infsoln.com>
2018-09-10 20:00:26 -0600

    New: gulpfile
    Gulp file including function 'styles' for compiling from /sass/ directory into the /css/ directory.

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+        return;
+});
+
+gulp.task('default',function() {
+    gulp.watch('./sass/**/*.scss',['styles']);
+    return;
+});
+
